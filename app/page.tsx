e13a9b3d74d16f67330bc6ad702f7a1d7d008aba
Hein12345 <heinzawphyoe2@gmail.com>
2025-05-08 21:53:50 +0700

    fix: client side render

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,28 @@
+"use client";
+
 import About from "@/components/elements/About";
 import Hero from "@/components/elements/Hero";
+import MyScores from "@/components/elements/MyScores";
 import Nav from "@/components/elements/Nav";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const page = () => {
+  const [client, setClient] = useState(false);
+
+  useEffect(() => {
+    setClient(true);
+  }, []);
   return (
-    <main className="">
-      <div>
-        <Nav />
-        <Hero />
-        <About />
-      </div>
-    </main>
+    client && (
+      <main className="">
+        <div>
+          <Nav />
+          <Hero />
+          <MyScores />
+          <About />
+        </div>
+      </main>
+    )
   );
 };
 
